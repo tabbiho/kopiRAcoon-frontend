308801192634542e92a84156ec1daaaf4ec17a88
cheenaeng <cheena94sing@gmail.com>
2022-05-25 00:00:43 +0800

    added animation package and added some basic animation

--- src/components/coffee-diagram/CoffeeDiagramMain.jsx
+++ src/components/coffee-diagram/CoffeeDiagramMain.jsx
@@ -2,6 +2,7 @@
 import React, { useContext } from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
+import { motion, AnimatePresence } from 'framer-motion';
 import AppContext from '../../functions.jsx';
 
 function coffeeDiagramMain() {
@@ -15,13 +16,31 @@ function coffeeDiagramMain() {
     }
   };
 
+  console.log(appState);
+
   return (
     <>
       <h1> Coffee</h1>
-      <div className="coffee-wrapper">
+      <AnimatePresence>
+        {appState.ice
+          && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ y: 15, opacity: 1 }}
+              transition={{ duration: 1 }}
+              exit={{ opacity: 0, transition: 0.1 }}
+              className="ice-image"
+            >
+              {' '}
+              ICE IMAGE
+            </motion.div>
+          )}
+      </AnimatePresence>
+      <motion.div initial={{ y: -20 }} animate={{ y: 0 }} transition={{ duration: 1 }} className="coffee-wrapper">
         <div className="water-layer" style={{ height: `${waterLayerProportion}%` }}>water</div>
         <div className="coffee-layer" style={{ height: `${coffeeLayerProportion}%` }}>coffee</div>
-      </div>
+      </motion.div>
+      <div className="table-div"> </div>
       <Box width={300}>
         <Slider aria-label="Default" valueLabelDisplay="auto" onChange={handleCoffeeLayer} value={coffeeLayerProportion} />
       </Box>
